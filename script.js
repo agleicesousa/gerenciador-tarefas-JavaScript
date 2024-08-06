a8cfaa830f1f3039db505118b0699249947a1c8d
agleicesousa <agleicesousa98@gmail.com>
2024-08-06 13:14:40 -0300

    fix: ajuste modal e overlay

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function notaTarefa() {
+const overlay = document.querySelector('#overlay');
+const modal = document.querySelector('.modal')
+
+function novaTarefa() {
     overlay.classList.add('active')
+    modal.classList.add('active')
 }
+
+// overlay.addEventListener('click', (e) => {
+//     console.log(e.target)
+//     overlay.classList.remove('active')
+//     modal.classList.remove('active')
+// })
